9b9de42ca1aacab2c6572243ef892ec66f34aadb
ryskooo91 <ruskodegz@gmail.com>
2025-06-09 01:01:56 +0200

    config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    apiKey: "sk_test_fakekey1234567890",
+    awsUser: "AKIAEXAMPLEJSKEY0987",
+    awsPass: "JsSecretKey/++",
+    smtpHost: "email-smtp.us-west-2.amazonaws.com",
+    smtpUser: "AKIAEXAMPLEJSKEY0987",
+    smtpPassword: "FakeJsSmtpPassword"
+};
